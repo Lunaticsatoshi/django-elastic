d1de69239e2e3684db339d8f20a3425ac28c65a3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-05 20:54:10 +0530

    :hammer: Adding dev script

---
+++ docker/server/run.dev.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+
+echo "==> $(date +%H:%M:%S) ==> Migrating Django models... "
+python manage.py migrate --noinput
+
+# echo "==> $(date +%H:%M:%S) ==> Initializing Django admin account.. "
+# python manage.py initadmin
+
+echo "==> $(date +%H:%M:%S) ==> Running server... "
+python manage.py runserver 0.0.0.0:8000
