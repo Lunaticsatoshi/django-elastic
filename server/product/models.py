4dc12b7bb837b40a3ed6d0a63e97a6a7ad57668c
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-07 20:16:56 +0530

    :bricks: Updating product model

--- server/product/models.py
+++ server/product/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     name = models.CharField(max_length=100, db_index=True, null=False, blank=False)
-    price = models.DecimalField(decimal_places=10, null=True, blank=True)
+    price = models.DecimalField(max_digits=19, decimal_places=10, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     quantity = models.IntegerField(null=True, blank=True, default=0)
     created_at = models.DateTimeField(auto_now_add=True)
