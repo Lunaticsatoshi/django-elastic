b0ba01961442a94b9a9e0bcc46640d15d0f5a4f3
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-07 21:23:46 +0530

    :gear: Adding search view

--- server/product/views.py
+++ server/product/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.views import GenericAPIView
+from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
+from django_elasticsearch_dsl_drf.filter_backends import SearchFilterBackend
+
+from .models import Product
+from .serializers import ProductSerializer, ProductDocumentSerializer
+from .documents import ProductDocument
 
 # Create your views here.
+
+class MovieSearchWithESViewSet(DocumentViewSet):
+    """search products via elastic search index"""
+    document = ProductDocument
+    serializer_class = ProductDocumentSerializer
+    
+    filter_backends = [SearchFilterBackend]
+    
+    search_fields = [
+        'name',
+        'description',
+    ]
+    
+    filter_fields = {
+        'name': 'name',
+    }
