2a34ea7164a3f67d88f663bcf1860dde4cd2d98f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-08 20:34:58 +0530

    :railway_track: Updating product search query url

--- server/product/urls.py
+++ server/product/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ProductSearchWithESViewSet, get_all_products, ProductCreateApiView, ProductUpdateApiView, ProductDeleteApiView
 
 urlpatterns = [
-    path('products/', ProductSearchWithESViewSet.as_view(), name='product-list'),
+    path('products/<str:query>', ProductSearchWithESViewSet.as_view(), name='product-list'),
     path('products/all/', get_all_products, name='product-list-all'),
     path('products/create/', ProductCreateApiView.as_view(), name='product-create'),
     path('products/<int:pk>/update/', ProductUpdateApiView.as_view(), name='product-update'),
