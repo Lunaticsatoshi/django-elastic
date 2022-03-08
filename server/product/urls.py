f2424bcb9229ae3a3660a9613a6359331f2c9b0f
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-08 20:21:06 +0530

    :railway_track: Updating product urls

--- server/product/urls.py
+++ server/product/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from .views import ProductSearchWithESViewSet, get_all_products, ProductCreateApiView, ProductUpdateApiView, ProductDeleteApiView
 
 urlpatterns = [
-    path('products/', ProductSearchWithESViewSet),
-    path('products/all/', get_all_products),
-    path('products/create/', ProductCreateApiView.as_view()),
-    path('products/<int:pk>/update/', ProductUpdateApiView.as_view()),
-    path('products/<int:pk>/delete/', ProductDeleteApiView.as_view()),
+    path('products/', ProductSearchWithESViewSet.as_view(), name='product-list'),
+    path('products/all/', get_all_products, name='product-list-all'),
+    path('products/create/', ProductCreateApiView.as_view(), name='product-create'),
+    path('products/<int:pk>/update/', ProductUpdateApiView.as_view(), name='product-update'),
+    path('products/<int:pk>/delete/', ProductDeleteApiView.as_view(), name='product-delete'),
 ]
