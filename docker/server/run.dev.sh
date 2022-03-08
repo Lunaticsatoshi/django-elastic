6aa7a8e2706ab1e615a3cb628aaefc0caa69de38
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-08 20:05:55 +0530

    :hammer: Adding script for django admin and rebuild elastic to run

--- docker/server/run.dev.sh
+++ docker/server/run.dev.sh
@@ -3,5 +3,13 @@
 echo "==> $(date +%H:%M:%S) ==> Migrating Django models... "
 python manage.py migrate --noinput
 
+echo "==> $(date +%H:%M:%S) ==> Initializing rebuild elastic search script .. "
+
+python manage.py runscript rebuildelastic
+
+echo "==> $(date +%H:%M:%S) ==> Initializing Django admin account.. "
+
+python manage.py initadmin
+
 echo "==> $(date +%H:%M:%S) ==> Running server... "
 python manage.py runserver 0.0.0.0:8000
