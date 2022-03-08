95eb3a0cf4c21c60626dd3668d8c6c665bce39ab
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-08 20:07:27 +0530

    :pencil2: fixing typo in import

--- server/product/views.py
+++ server/product/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.views import GenericAPIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
