e56de7ca3468d682642e59ee0bfa6fdcc20f9822
Lunaticsatoshi <deysarkersudeep@gmail.com>
2022-03-09 21:05:41 +0530

    :sparkles: Adding redux store

---
+++ client/src/constants.js
@@ -0,0 +1,7 @@
+export const PRODUCTS_LOADED = "PRODUCTS_LOADED";
+export const TOGGLE_LOADING = "TOGGLE_LOADING";
+export const PRODUCT_ADDED = "TASK_ADDED";
+export const PRODUCT_UPDATED = "PRODUCT_UPDATED";
+export const PRODUCT_DELETED = "PRODUCT_DELETED";
+export const SHOW_ERROR = "SHOW_ERROR";
+export const HIDE_ERROR = "HIDE_ERROR";
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,54 @@
-import './style.css'
-import '@riotjs/hot-reload'
-import { mount } from 'riot'
-import registerGlobalComponents from './register-global-components'
+import "./style.css";
+import "@riotjs/hot-reload";
+import { mount } from "riot";
+import registerGlobalComponents from "./register-global-components";
+
+import { applyMiddleware, compose, createStore } from "redux";
+import thunk from "redux-thunk";
+
+import {
+	PRODUCTS_LOADED,
+	TOGGLE_LOADING,
+	PRODUCT_ADDED,
+	SHOW_ERROR,
+	HIDE_ERROR,
+} from "./constants";
+
+const reducer = function (state = { tasks: [] }, action) {
+	console.log(action);
+	switch (action.type) {
+		case PRODUCTS_LOADED:
+			return Object.assign({}, state, { tasks: action.data });
+
+		case TOGGLE_LOADING:
+			return Object.assign({}, state, { isLoading: action.data });
+
+		case PRODUCT_ADDED:
+			return Object.assign({}, state, {
+				tasks: state.tasks.concat(action.data),
+			});
+
+		case SHOW_ERROR:
+			return Object.assign({}, state, {
+				isError: true,
+				errorMessage: action.data,
+			});
+
+		case HIDE_ERROR:
+			return Object.assign({}, state, { isError: false, errorMessage: "" });
+
+		default:
+			return state;
+	}
+};
+
+//const reduxStore = createStore(reducer);
+const createStoreWithMiddleware = compose(applyMiddleware(thunk))(createStore);
+
+let reduxStore = createStoreWithMiddleware(reducer);
 
 // register
-registerGlobalComponents()
+registerGlobalComponents();
 
 // mount all the global components found in this page
-mount('[data-riot-component]')
+mount("[data-riot-component]", { store: reduxStore });
